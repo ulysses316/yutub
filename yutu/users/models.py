fa922d47a9cd604db0a4caaa0ce02a58b5457e6d
ulysses316 <ulises316@live.com.mx>
2019-12-06 02:30:08 -0600

    Agrego proyecto de django base, modelos de user y de video creados (Corregir bug de videos.)

---
+++ yutu/users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Users(User, models.Model):
+    username = User.username
+    password = User.password
+    email = User.email
+    first_name = User.first_name
+    last_name = User.last_name
+    staf = User.is_staff
+    last_login = User.last_login
+    date_joined = User.date_joined
+    age = models.DateField(auto_now=True)
+    image_profile = models.ImageField(upload_to='uploads/image_profile', blank=True, default='default.jpeg')
+    
+    def __str__(self):
+        return self.username
---
+++ yutu/videos3/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from users.models import Users
+# Create your models here.
+
+class Videos(models.Model):
+    name = models.CharField(max_length = 30)
+    description = models.TextField()
+    verbose_name = models.CharField(max_length=30)
+    visibility = models.BooleanField(default= True)
+    rated = models.BooleanField(default = False)
+    date = models.DateTimeField(auto_now_add= True)
+    video = models.FileField(upload_to='uploads/videos/%Y/%m/%d/', default='default.mp3')
+
+
+    def __str__(self):
+        return self.name
